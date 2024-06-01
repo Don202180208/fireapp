75179efaefad3c509caddc274c285d5d45f9f8fd
Don202180208 <202180208@psu.palawan.edu.ph>
2024-06-01 19:16:56 +0800

    Modification

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -5,11 +5,10 @@ from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyM
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", HomePageView.as_view(), name='home'),
-    path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
-    path('chart/', PieCountbySeverity, name='chart'),
-    path('linecountChart/', LineCountbyMonth, name='chart'),
-    path('multilineChart/', MultilineIncidentTop3Country, name='chart'),
-    path('multiBarChart/', multipleBarbySeverity, name='chart'),
-    path('map_station/', multipleBarbySeverity, name='map-station'),
+    path('dashboard_chart/', ChartView.as_view(), name='dashboard-chart'),
+    path('pie-severity/', PieCountbySeverity, name='pie-severity'),
+    path('line-count-chart/', LineCountbyMonth, name='line-count-chart'),
+    path('multiline-chart/', MultilineIncidentTop3Country, name='multiline-chart'),
+    path('multiple-bar-severity/', multipleBarbySeverity, name='multiple-bar-severity'),
+    path('map_station/', map_station, name='map-station'),
 ]
-
